562f14d56f1bdecb073142e6b07a04a0eeec8a79
Charles Palmer <charleswcpalmer@gmail.com>
2019-09-20 17:02:05 +0000

    Sketching out some types for things to mix

--- src/mixdown.ts
+++ src/mixdown.ts
@@ -1,5 +1,34 @@
 // A Web Audio based mixer for games.
 
+interface Sample {
+    kind : "sample";
+    asset : string;
+    gain : number;
+    startSample : number;
+    endSample : number;
+}
+
+interface Sfx {
+    kind : "sfx";
+    samples : [Sample];
+    priority : "high" | "medium" | "low";
+}
+
+interface Music {
+    kind : "music";
+    asset : string;
+    gain : number;
+}
+
+interface Index {
+    readonly index : number;
+    readonly generation : number;
+}
+
+type Playable = Sample | Sfx | Music;
+
+type IndexResult = "success" | "doesNotExist" | "oldGeneration";
+
 class Mixdown {
     context : AudioContext = new AudioContext();
     assetMap : Record<string, AudioBuffer> = {};
@@ -26,9 +55,34 @@ class Mixdown {
         
     }
 
+    play(playable : Playable) : Index | undefined {
+        return undefined;
+    }
+
+    stop(index : Index) : IndexResult {
+        return "doesNotExist";
+    }
+
+    fadeIn(index : Index, value : number, duration : number) : IndexResult {
+        return "doesNotExist";
+    }
+
+    fadeOut(index : Index, value : number, duration : number) : IndexResult {
+        return "doesNotExist";
+    }
+
+    gain(index : Index, value : number) : IndexResult {
+        return "doesNotExist";
+    }
+
+    balance(index : Index, value : number) : IndexResult {
+        return "doesNotExist";
+    }
+
     loadAsset(name : string, path : string) {
         // todo: make sure we're loading something we support
         // todo: return promise from this to cover user callbacks?
+        // todo: xmlhttprequest for backwards compat?
 
         fetch(path).then(response => response.arrayBuffer())
         .then(data => this.context.decodeAudioData(data))
