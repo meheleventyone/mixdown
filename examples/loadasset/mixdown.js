920972eb7eb71c83d9bff1eb7d91fd9126f7a394
Charles Palmer <charleswcpalmer@gmail.com>
2019-09-23 21:52:35 +0000

    Update load asset example to load asset after context is created and play it as a sample immediately after audio context starts running

--- examples/loadasset/mixdown.js
+++ examples/loadasset/mixdown.js
@@ -1,9 +1,77 @@
 "use strict";
 // A Web Audio based mixer for games.
+var Priority;
+(function (Priority) {
+    Priority[Priority["Low"] = 0] = "Low";
+    Priority[Priority["Medium"] = 1] = "Medium";
+    Priority[Priority["High"] = 2] = "High";
+})(Priority || (Priority = {}));
+var GenerationalArray = /** @class */ (function () {
+    function GenerationalArray(initialSize, noResize) {
+        if (noResize === void 0) { noResize = true; }
+        this.generation = [];
+        this.data = [];
+        this.freeList = [];
+        this.noResize = noResize;
+        for (var i = 0; i < initialSize; ++i) {
+            this.generation[i] = 0;
+            this.data[i] = null;
+            this.freeList.push(i);
+        }
+    }
+    GenerationalArray.prototype.add = function (data) {
+        var index = -1;
+        if (this.freeList.length == 0) {
+            if (this.noResize) {
+                return undefined;
+            }
+            index = this.data.length;
+        }
+        else {
+            index = this.freeList.pop();
+        }
+        index = index;
+        this.data[index] = data;
+        return { index: index, generation: this.generation[index] };
+    };
+    GenerationalArray.prototype.get = function (handle) {
+        if (handle.generation != this.generation[handle.index]) {
+            return undefined;
+        }
+        var index = handle.index;
+        if (this.data[index] == null) {
+            return undefined;
+        }
+        return this.data[index];
+    };
+    GenerationalArray.prototype.remove = function (handle) {
+        if (handle.generation != this.generation[handle.index]) {
+            return undefined;
+        }
+        var index = handle.index;
+        this.generation[index] += 1;
+        this.data[index] = null;
+        this.freeList.push(index);
+    };
+    GenerationalArray.prototype.valid = function (handle) {
+        return handle.generation == this.generation[handle.index];
+    };
+    GenerationalArray.prototype.freeSlots = function () {
+        return this.freeList.length;
+    };
+    return GenerationalArray;
+}());
 var Mixdown = /** @class */ (function () {
-    function Mixdown() {
+    function Mixdown(maxVoices, slopSize) {
+        if (maxVoices === void 0) { maxVoices = 32; }
+        if (slopSize === void 0) { slopSize = 4; }
         this.context = new AudioContext();
         this.assetMap = {};
+        this.maxVoices = maxVoices;
+        this.slopSize = 4;
+        this.masterGain = this.context.createGain();
+        this.masterGain.connect(this.context.destination);
+        this.voices = new GenerationalArray(maxVoices);
     }
     Mixdown.prototype.suspend = function () {
         if (this.context.state === "suspended") {
@@ -21,6 +89,57 @@ var Mixdown = /** @class */ (function () {
     Mixdown.prototype.rebuild = function () {
     };
     Mixdown.prototype.play = function (playable) {
+        switch (playable.kind) {
+            case "sample":
+                return this.playSample(playable);
+            case "sfx":
+                return this.playSfx(playable);
+            case "music":
+                return this.playMusic(playable);
+        }
+        return undefined;
+    };
+    Mixdown.prototype.playSample = function (sample) {
+        var _this = this;
+        var buffer = this.assetMap[sample.asset];
+        if (!buffer) {
+            return undefined;
+        }
+        if (this.voices.freeSlots() == 0) {
+            // todo priority search
+            return undefined;
+        }
+        var ctx = this.context;
+        var source = ctx.createBufferSource();
+        source.buffer = buffer;
+        var balance = ctx.createStereoPanner();
+        source.connect(balance);
+        var gain = ctx.createGain();
+        balance.connect(gain);
+        gain.gain.setValueAtTime(sample.gain, ctx.currentTime);
+        gain.connect(this.masterGain);
+        source.start();
+        var handle = this.voices.add({ gain: gain, balance: balance, source: source, priority: sample.priority });
+        if (handle) {
+            source.onended = function () { _this.ended(handle); };
+        }
+        return handle;
+    };
+    Mixdown.prototype.ended = function (handle) {
+        var voice = this.voices.get(handle);
+        if (!voice) {
+            return;
+        }
+        voice.source.disconnect();
+        voice.source.buffer = null;
+        voice.gain.disconnect();
+        voice.balance.disconnect();
+        this.voices.remove(handle);
+    };
+    Mixdown.prototype.playSfx = function (sfx) {
+        return undefined;
+    };
+    Mixdown.prototype.playMusic = function (music) {
         return undefined;
     };
     Mixdown.prototype.stop = function (index) {
