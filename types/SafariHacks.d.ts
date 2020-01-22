ea593dc453e653a73bf1986eadd78be50c09fa06
Charles Palmer <charleswcpalmer@gmail.com>
2020-01-22 21:27:15 +0000

    Docs

--- types/SafariHacks.d.ts
+++ types/SafariHacks.d.ts
@@ -1,17 +1,68 @@
+/**
+ * A set of utility functions and classes to help work around deficiencies in the Safari WebAudio implementation.
+ * @packageDocumentation
+ */
+/**
+ * Part of the [[MixdownStereoPannerNode]] discriminated union.
+ *
+ * This is for the Safari implementation using the WebAudio PannerNode.
+ */
 interface SafariNode {
     kind: "safari";
     panner: PannerNode;
 }
+/**
+ * Part of the [[MixdownStereoPannerNode]] discriminated union.
+ *
+ * This is for all non-Safari implementations using the WebAudio StereoPannerNode.
+ */
 interface StereoNode {
     kind: "stereopanner";
     stereoPanner: StereoPannerNode;
 }
+/**
+ * A discriminated union representing the different implementations of the [[MixdownStereoPanner]]
+ */
+declare type MixdownStereoPannerNode = SafariNode | StereoNode;
+/**
+ * MixdownStereoPanner is a wrapper over the cross-platform implementation details of StereoPannerNode.
+ *
+ * On Safari it represents the StereoPannerNode using the PannerNode using the 'equalpower' model.
+ * To correctly pan the sound it is moved along the x-axis between -1 and 1 from left to right.
+ * To keep the loudness equivalent for all positions as you would expect in purely stereo output the
+ *  distance is kept at 1 unit by offsetting the sound forward along the z-axis by 1 - abs(panValue).
+ *
+ * On other platforms it uses the standard StereoPannerNode.
+ *
+ * Note: On Safari this is probably not going to mix well with anything that moves
+ * the listener position around, at that point we would need to adjust all
+ * MixdownStereoPanner nodes to be offset from that position
+ */
 export declare class MixdownStereoPanner {
-    _panner: SafariNode | StereoNode;
+    /**
+     * @ignore
+     * */
+    _panner: MixdownStereoPannerNode;
+    /**
+      * @ignore
+      * */
     _pan: number;
+    /**
+     *  @returns The current value of the pan property.
+     */
     get pan(): number;
+    /**
+     * @param value The value to set pan to in the underlying implementation. This is clamped in the range -1 to 1 inclusive.
+     */
     set pan(value: number);
+    /**
+     *
+     * @param context The AudioContext that [[Mixdown]] is using.
+     */
     constructor(context: AudioContext);
+    /**
+     * @returns A reference to the AudioNode being used by the underlying implementation.
+     */
     getAudioNode(): AudioNode;
 }
 export {};
--- types/Utility.d.ts
+++ types/Utility.d.ts
@@ -1,11 +1,46 @@
+/**
+ *  Value<T> is a part of the Result<T, E> discriminated union, it represents a valid value of T being returned.
+ *  @typeParam T The type of the value that will be contained in Value<T>.
+ */
 interface Value<T> {
     kind: "value";
     value: T;
 }
+/**
+ *  Error<T> is a part of the Result<T, E> discriminated union, it represents an error of T being returned.
+ *  @typeParam T The type of the error that will be contained in Error<T>.
+ */
 interface Error<T> {
     kind: "error";
     error: T;
 }
+/**
+ * A Result type representing a discriminated union of a successful value being returned from a function or an error.
+ *
+ * Checking for value or error:
+ * ```typescript
+ * const result = someFunctionThatReturnsResult();
+ * if (result.kind === "value") {
+ *      // can use result.value
+ * } else {
+ *      // can use result.error
+ * }
+ * ```
+ * @typeParam T The type of the value to be returned.
+ * @typeParam E The type of the error to be returned in the event that the function fails.
+ */
 export declare type Result<T, E> = Value<T> | Error<E>;
+/**
+ * An Optional type representing a value that may or may not be set.
+ *
+ * Simple to check for a valid value:
+ * ```typescript
+ * if (optionalValue !== undefined) {
+ *      // do something
+ * }
+ * ```
+ *
+ * @typeParam T the type of the value if set.
+ */
 export declare type Optional<T> = T | undefined;
 export {};
